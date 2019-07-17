a88c70e310f341449d1b6253cb24daa3eba5f66f
在那遥远的瞬间 <z351622948@163.com>
2019-07-17 16:07:10 +0800

    修复在Linux因文件顺序问题引发的循环依赖问题

--- src/main/java/com/db/homepage/common/utils/BizCache.java
+++ src/main/java/com/db/homepage/common/utils/BizCache.java
@@ -11,7 +11,7 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
 @Component
 @SuppressWarnings("unchecked")
 @Slf4j
-@Lazy
+@Order
 public class BizCache {
     /**
      * index缓存key
